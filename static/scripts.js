2cddd5aa8370a8aba12ccffb6027b62a9bad741b
ahnaf21 <aua001@ucsd.edu>
2018-04-09 20:04:31 -0700

    Ahnaf

--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,7 @@
+function Subscribe() {
+    alert("Congratutions! You've Subscribed!");
+}
+function NOW() {
+    alert("The Pharo is a for profit organization that serves B2C bikers who need high-protection, gps tracking and unlocking mechanism via smart devices. We will generate revenue using direct sales model.");
+}
 
