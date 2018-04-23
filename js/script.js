922ad93ca3c912df62fe62ac38ee34957e33b8c7
LAPTOP-SEQDTQI2\Justin Tran <jtt049@ucsd.edu>
2018-04-23 11:11:30 -0700

    justin Tran website

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function Subscribe() {
-    alert("Congratutions! You've Subscribed!");
-}
-function NOW() {
-    alert("The Pharo is a for profit organization that serves B2C bikers who need high-protection, gps tracking and unlocking mechanism via smart devices. We will generate revenue using direct sales model.");
-}
-
