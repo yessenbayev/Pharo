fba678fd6bc82fa83f20e0f6aaf2705969c46f6d
Alan' Yessenbayev <alan.yessenbayev@gmail.com>
2018-04-24 20:43:37 -0700

    Wait

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ mail = Mail(app)
 # ##### DB SETUP #####
 
 class Emails(db.Model):
-	__tablename__ = "emails"
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120),unique=False)
 	email = db.Column(db.String(120),unique=True)
